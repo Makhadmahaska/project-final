d98a7a4287d89c451f19c8832e76f523a577f847
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-31 11:17:43 +0200

    added eslint and edited and fixed small errors

---
+++ backend/eslint.config.ts
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{js,ts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      "@typescript-eslint/no-namespace": "off",
+    },
+  },
+]);
